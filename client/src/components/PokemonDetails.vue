40dc108d882621b640a7669aba0eb11fde5b9d5b
Marc Downie <marcdownie3@hotmail.com>
2020-12-05 15:16:09 +0000

    started pokemon details

---
+++ client/src/components/PokemonDetails.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="pokemon-detail">
+    <h3>{{ pokemon.forms.name }}</h3>
+    <p>{{ pokemon.sprites.front_default }}</p>
+    <p>  </p>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'pokemon-detail'
+}
+</script>
+
+<style>
+
+</style>
