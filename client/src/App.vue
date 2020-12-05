55e3ee46613f9ae67c591c752dfe7af169dca515
Emma Hack <ehack78@gmail.com>
2020-12-05 11:57:18 +0000

    initial commit

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,30 @@
 <template>
+<div>
+    test
+</div>
 
 </template>
 
 <script>
+
 export default {
+    name: 'app',
+    components: {
+
+    },
+    data (){
+        return {
+            pokemon: []
+        };
+    },
+    mounted(){
+        this.fetchPokemon();
+    },
+    methods: {
+        fetchPokemon(){
+            
+        }
+    }
 
 }
 </script>
