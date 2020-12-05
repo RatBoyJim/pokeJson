fbd9b4a820de687ce26009cf33ec8fe1d2e215df
Michael Anderson <ratboyjim@gmail.com>
2020-12-05 10:29:56 +0000

    first commit

---
+++ client/src/components/PokemonBattle.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="battle">
+      <button :pokemon1="pokemon[0]" :pokemon2="pokemon[1]" v-on:click="attackByFirstPokemon">{{pokemon1.moves[0].move.name}}</button>
+  </div>
+</template>
+
+<script>
+
+import Pokemon from './PokemonList'
+
+export default {
+	name: 'battle-result',
+	data() {
+		return {
+            pokemon1Moves: [],
+            pokemon2Moves: []
+		}
+    },
+    mounted() {
+    this.fetchMovesP1();
+    this.fetchMovesP2();
+    },
+	methods: {
+        fetchMovesP1() {
+            let movesURL = pokemon1.moves[0].move.url
+            fetch('movesURL')
+            .then(response => response.json())
+            .then(data => this.pokemon1Moves = data)
+    },
+        fetchMovesP2() {
+            let movesURL = pokemon2.moves[0].move.url
+            fetch('movesURL')
+            .then(response => response.json())
+            .then(data => this.pokemon2Moves = data)
+    },
+		attackByFirstPokemon(pokemon1, pokemon2){
+			return pokemon2.stats[0].base_stat = pokemon2.stats[0].base_stat - pokemon1Moves.moves[0].pp
+			}
+	}
+    
+}
+
+</script>
+
+<style>
+
+</style>
