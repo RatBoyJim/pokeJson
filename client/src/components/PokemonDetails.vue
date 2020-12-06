6c398513c1774a937794584608938c93b2208a30
Michael Anderson <ratboyjim@gmail.com>
2020-12-06 22:04:31 +0000

    some styling done

--- client/src/components/PokemonDetails.vue
+++ client/src/components/PokemonDetails.vue
@@ -1,8 +1,11 @@
 <template>
 <div class="pokemon-detail">
+    <img class="lrg-pokemon" :src="pokemonDetails.sprites.front_default " />
     <h3>Name: {{ pokemonDetails.name }}</h3>
     <h4>Height: {{ pokemonDetails.height}}</h4>
-    <!-- <img class="lrg-pokemon" :src="pokemon.sprites.front_default " /> -->
+    <h4>{{pokemonDetails.moves[0].move.name}}</h4>
+    <h4>{{pokemonDetails.moves[1].move.name}}</h4>
+    
 </div>
 </template>
 
@@ -29,6 +32,15 @@ export default {
 </script>
 
 <style>
+    .pokemon-detail {
+        border: solid 10px rgb(255, 217, 0);
+        border-radius: 5px;
+        width: 150px;
+        background-color: lightgoldenrodyellow;
+        padding: 10px;
+        font-family: 'Fira Sans', sans-serif;
+        background-image: url("https://cutewallpaper.org/21/pokemon-card-backgrounds/Backround-Pokemon-Card-Related-Keywords-and-Suggestions-.png");
+    }
     .lrg-pokemon {
         height: 100px;
     }
