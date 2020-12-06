4fb04a419e82c4429e9c1cce3b8ba25ae4cffb60
Michael Anderson <ratboyjim@gmail.com>
2020-12-06 15:23:22 +0000

    merge commit

--- client/src/components/PokemonList.vue
+++ client/src/components/PokemonList.vue
@@ -4,7 +4,7 @@
     <label for="pokemon_select">Select a Pokemon:</label>
     <select id="pokemon_select" v-model="selectedPokemon">
       <option disabled value="">Select a Pokemon</option>
-      <option v-for="(character, index) in pokemon" :pokemon="pokemon" :value="pokemon" :key="index">{{pokemon[index].name}}</option>
+      <option v-for="(pokeman, index) in pokemon" :value="pokemon" :key="index">{{pokemon[index].name}}</option>
     </select>
   </div>
 </template>
