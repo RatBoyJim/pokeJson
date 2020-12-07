05d2e0795b49fb73a564e152a0a06e334c6e5b69
Michael Anderson <ratboyjim@gmail.com>
2020-12-07 11:22:36 +0000

    merge commit

--- client/src/App.vue
+++ client/src/App.vue
@@ -26,7 +26,8 @@ export default {
         return {
             pokemon: [],
             selectedPokemon: null,
-            pokemonDetails: null
+            pokemonDetails: null,
+
             
         };
     },
