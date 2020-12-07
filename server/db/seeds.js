c9d0be5cf64f0a1a105526fd902bb70c248684c2
Emma Hack <ehack78@gmail.com>
2020-12-07 20:04:39 +0000

    seeds.js filled in

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,19 +1,102 @@
 use pokemon_team;
 db.dropDatabase();
 
-// below is a dummy seed that I used to test db was working
-// can be uncommented if we want to check anything else or deleted when we're ready to seed for real
-
 db.pokemon.insertMany([
     {
-        name: "charmander",
-        wins: 1,
-        losses: 3
+        name: "bulbasaur",
+        wins: 0,
+        losses: 0
     },
     {
-        name: "pikachu",
-        wins: 5,
+        name: "ivysaur",
+        wins: 0,
+        losses: 0
+    },
+    {
+        name: "venusaur",
+        wins: 0,
+        losses: 0
+    },
+    {
+        name: "charmeleon",
+        wins: 0,
+        losses: 0
+    },
+    {
+        name: "charizard",
+        wins: 0,
+        losses: 0
+    },
+    {
+        name: "squirtle",
+        wins: 0,
+        losses: 0
+    },
+    {
+        name: "wartortle",
+        wins: 0,
+        losses: 0
+    },
+    {
+        name: "blastoise",
+        wins: 0,
+        losses: 0
+    },
+    {
+        name: "caterpie",
+        wins: 0,
+        losses: 0
+    },
+    {
+        name: "metapod",
+        wins: 0,
+        losses: 0
+    },
+    {
+        name: "butterfree",
+        wins: 0,
+        losses: 0
+    },
+    {
+        name: "weedle",
+        wins: 0,
+        losses: 0
+    },
+    {
+        name: "kakuna",
+        wins: 0,
+        losses: 0
+    },
+    {
+        name: "beedrill",
+        wins: 0,
+        losses: 0
+    },
+    {
+        name: "pidgey",
+        wins: 0,
+        losses: 0
+    },
+    {
+        name: "pidgeotto",
+        wins: 0,
+        losses: 0
+    },
+    {
+        name: "pidgeot",
+        wins: 0,
+        losses: 0
+    },
+    {
+        name: "rattata",
+        wins: 0,
+        losses: 0
+    },
+    {
+        name: "raticate",
+        wins: 0,
         losses: 0
     }
+
 
 ])
