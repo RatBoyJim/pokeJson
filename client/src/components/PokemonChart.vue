53a82566347076f46e602e18d1c05967799b407d
Emma Hack <ehack78@gmail.com>
2020-12-08 11:08:45 +0000

    chart hardcoded data working

--- client/src/components/PokemonChart.vue
+++ client/src/components/PokemonChart.vue
@@ -1,11 +1,22 @@
 <template>
-  <p>test</p>
+    <GChart type="PieChart" :data="chartData" />
 </template>
 
 <script>
 import { GChart } from 'vue-google-charts'
 
 export default {
+    data () {
+        return{
+            chartData:[
+                ['Pokemon', 'wins'],
+                ['Pikachu', 5],
+                ['Charmander', 2],
+                ['Bulbasaur', 4],
+                ['Weedle', 6]
+            ]
+        }
+    },
     components: {
         GChart
     }
